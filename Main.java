39b09da3db03b23fd63c6bd5fee06d5fa5a97651
Ozgur Savascioglu <51115884+OzgurSavascioglu@users.noreply.github.com>
2025-04-15 15:43:35 +0300

    Update and rename Ozgur_Savascioglu.java to Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class Ozgur_Savascioglu {
+public class Main {
 
 	public static void main (String[] args) throws FileNotFoundException{
 		
